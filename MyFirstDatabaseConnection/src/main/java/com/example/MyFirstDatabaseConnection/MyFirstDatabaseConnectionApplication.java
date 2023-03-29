a30ea8683bf7be0dac6e9e3792b35402700c4bfb
shashank <shashank944533@gmail.com>
2023-03-29 12:45:01 +0530

    connection to postgresql

---
+++ MyFirstDatabaseConnection/src/main/java/com/example/MyFirstDatabaseConnection/MyFirstDatabaseConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstDatabaseConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstDatabaseConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstDatabaseConnectionApplication.class, args);
+	}
+
+}
